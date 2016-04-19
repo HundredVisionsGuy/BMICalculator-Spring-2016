a408ded4f8127cc545f59917aba8253578da1795
hundredvisionsguy <winikkc@hsd.k12.or.us>
2016-04-19 13:31:33 -0700

    restored files to pre-Model branch

--- app/src/main/java/com/hundredvisions/bmicalculator/MainActivity.java
+++ app/src/main/java/com/hundredvisions/bmicalculator/MainActivity.java
@@ -37,23 +37,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 String results = "Results: ";
-                int feet;
                 // Check to make sure feet, inches, & pounds are set
-                String str_feet;
-                str_feet = edit_feet.getText().toString();
-                /*if (Model.isEmpty(str_feet)) {
-                    results = "Feet is empty";
-                }
-                else {
-                    results = "Feet is not empty";
-                }*/
-                try {
-                    feet = (Model.validateFeet(str_feet));
-                }
-                catch (IllegalArgumentException iae) {
-                    results = iae.getMessage();
-                }
-/*                if (edit_feet.getText().length() == 0) {
+                if (edit_feet.getText().length() == 0) {
                     edit_feet.setError("Please enter a #.");
                     results += "Oops! There was an error. ";
                 }
@@ -65,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 else {
                     results += "The length of feet is: " +
                             edit_feet.getText().length();
-                }*/
+                }
                 text_results.setText(results);
             }
         });
